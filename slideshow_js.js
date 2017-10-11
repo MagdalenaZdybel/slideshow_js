81d4a3baa80638005fa2993d21d635e6d24a4954
Magda <magda@motta.com.pl>
2017-10-11 22:59:36 +0200

    next commit

---
+++ slideshow_js.js
@@ -0,0 +1,26 @@
+window.onload = function()
+{
+  var slideShow = document.getElementById("slideShow");
+  
+  var imagesSrc = ["foto0.jpg", "foto1.jpg", "foto2.jpg"];
+  
+  for (var i=0; i < imagesSrc.length; i++)
+  {
+      var image = new Image();
+      
+      image.src = "img/"+imagesSrc[i];
+      
+      slideShow.appendChild(image);
+  }
+  
+  slideShow.childNodes[0].setAttribute("class","current");
+  var i = 0;
+  
+  setInterval(function()
+  {
+    slideShow.childNodes[i % imagesSrc.length].setAttribute("class","");
+    
+    slideShow.childNodes[(i+1) % imagesSrc.length].setAttribute("class","current");
+    i++;
+  }, 2000);
+};
